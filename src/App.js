1de44159649ba9dcfa91428fb4c0a527cdabd255
sophiebl <sophieboulaaouli@gmail.com>
2021-05-04 19:12:20 +0000

    Add New Restaurant

--- src/App.js
+++ src/App.js
@@ -65,19 +65,47 @@ const handleRestaurantRemove = (objectID) => {
   });
 };
 
-function App() {
-  const [open, setOpen] = useState(false);
+const handleRestaurantAdd = (inputs) => {
+  const objects = [
+    {
+      ...inputs,
+      area: "New York / Tri-State Area",
+      country: "US",
+      image_url: "https://www.opentable.com/img/restimages/99889.jpg",
+      mobile_reserve_url: "http://mobile.opentable.com/opentable/?restId=99889",
+      payment_options: ["AMEX", "MasterCard", "Visa"],
+      phone: "7185754033x",
+      postal_code: "11375",
+      price: 3,
+      reserve_url: "http://www.opentable.com/single.aspx?rid=99889",
+      state: "NY",
+      _geoloc: { lat: 40.710483, lng: -73.849684 },
+      stars_count: 4.5,
+      reviews_count: 36.0,
+      neighborhood: "Forest Hills",
+      phone_number: "(718) 575-4033",
+      price_range: "$31 to $50",
+      dining_style: "Casual Dining",
+      rounded_stars_count: 5,
+    },
+  ];
 
+  index
+    .saveObjects(objects, { autoGenerateObjectIDIfNotExist: true })
+    .then(({ objectIDs }) => {
+      toast.success("Restaurant is now added to the database", {
+        position: toast.POSITION.TOP_LEFT,
+      });
+    });
+};
+
+function App() {
   return (
     <ThemeProvider theme={theme}>
       <>
         <GlobalStyles />
         <InstantSearch searchClient={searchClient} indexName="restaurants">
           <Header />
-          <div>
-            <Burger open={open} setOpen={setOpen} />
-            <Filters open={open} index={index} />
-          </div>
           <div className="wrapper">
             <FiltersItem />
             <Results />
@@ -89,14 +117,34 @@ function App() {
 }
 
 function Header() {
+  const [open, setOpen] = useState(false);
+  const [openRestaurantForm, setOpenRestaurantForm] = useState(false);
+
   return (
     <header className="">
-      <h1>RestO</h1>
+      <div className="burger">
+        <Burger open={open} setOpen={setOpen} />
+        <Filters open={open} index={index} />
+      </div>
+      <h1>
+        <span className="logo">Restaurants</span>
+        <FontAwesomeIcon icon={faUtensils} />
+      </h1>
       <SearchBox
         translations={{
           placeholder: "Location, Restaurant or Cuisine",
         }}
       />
+      <div>
+        <PlusIcon
+          openRestaurantForm={openRestaurantForm}
+          setOpenRestaurantForm={setOpenRestaurantForm}
+        />
+        <RestaurantForm
+          openRestaurantForm={openRestaurantForm}
+          onRestaurantAdd={handleRestaurantAdd}
+        />
+      </div>
     </header>
   );
 }
